e67182cabf542237baebf7acac9ed4370adca03b
Julian Vogel <Julian.Vogel1@gmx.net>
2017-05-21 19:08:17 +0200

    Parser Klasse für Vorlagen für simulierte Räume inmplementiert

--- RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/Outline.java
+++ RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/Outline.java
@@ -1,11 +1,11 @@
 package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.RobotSimulation;
 
 import java.awt.geom.Point2D;
-import java.util.ArrayList;
+import java.util.List;
 
 public class Outline extends UltrasonicDetectable {
 
-	public Outline(ArrayList<Point2D> definingPoints) {
+	public Outline(List<Point2D> definingPoints) {
 		super(definingPoints);
 	}
 
--- RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/UltrasonicDetectable.java
+++ RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/UltrasonicDetectable.java
@@ -2,12 +2,13 @@ package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.RobotSimulation;
 
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.List;
 
 public abstract class UltrasonicDetectable {
-	protected ArrayList<Point2D> definingPoints;
+	protected List<Point2D> definingPoints;
 
-	public UltrasonicDetectable(ArrayList<Point2D> definingPoints) {
+	public UltrasonicDetectable(List<Point2D> definingPoints) {
 		super();
-		this.definingPoints = definingPoints;
+		this.definingPoints = new ArrayList<Point2D>(definingPoints);
 	}
 }
