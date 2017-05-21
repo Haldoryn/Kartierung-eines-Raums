db31c42da4da4c122744e143cb54dc86b3be1162
Julian Vogel <Julian.Vogel1@gmx.net>
2017-05-21 20:03:05 +0200

    Klasse Simulation hinzugefügt

---
+++ RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/Simulation.java
@@ -0,0 +1,14 @@
+package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.RobotSimulation;
+
+public class Simulation {
+	private SimulatedRoom room;
+
+	public Simulation(SimulatedRoom room) {
+		super();
+		this.room = room;
+	}
+
+	public SimulatedRoom getRoom() {
+		return room;
+	}
+}
