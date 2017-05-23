db019b84b74d0d9f0010a4b5f64c9bd313ccb294
Julian Vogel <Julian.Vogel1@gmx.net>
2017-05-23 20:33:10 +0200

    miglayout library mit eingecheckt

--- RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/Main.java
+++ RobotSimulation/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/RobotSimulation/Main.java
@@ -1,17 +1,12 @@
 package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.RobotSimulation;
 
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.IOException;
-import java.io.PrintStream;
-import java.net.URL;
-import java.util.Arrays;
 import java.util.Random;
 
-import javax.imageio.ImageIO;
 import javax.management.modelmbean.XMLParseException;
 import javax.swing.Timer;
 import javax.xml.parsers.ParserConfigurationException;
