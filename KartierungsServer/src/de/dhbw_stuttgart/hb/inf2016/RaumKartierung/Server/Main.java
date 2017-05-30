f2b33425c7db89240568832b16c0da124bdc20a3
Julian Vogel <Julian Vogel>
2017-05-30 12:55:19 +0200

    Server Projekt und Packages angelegt

---
+++ KartierungsServer/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/Server/Main.java
@@ -0,0 +1,10 @@
+package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.Server;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ KartierungsServer/src/de/dhbw_stuttgart/hb/inf2016/RaumKartierung/Server/VectorRoom/VectorRoom.java
@@ -0,0 +1,5 @@
+package de.dhbw_stuttgart.hb.inf2016.RaumKartierung.Server.VectorRoom;
+
+public class VectorRoom {
+
+}
